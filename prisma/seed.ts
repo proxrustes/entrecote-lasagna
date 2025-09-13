c64ebd3e29a86fe710581bee58343126571064a7
fabienstrauss <spaklhd@gmail.com>
2025-09-13 13:05:43 +0200

    edited tables and seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, UserType } from '../app/generated/prisma'
+import { PrismaClient, UserType } from '@/app/generated/prisma'
 import * as fs from 'fs'
 import * as path from 'path'
 
